2784b0a12a1b0e3595abe868786bffe8ebd3c4c5
Wyatt Channings <wyattchannings@gmail.com>
2022-10-26 16:10:53 +0800

    Feat(Users): Add user accounts.
    Add accounts-password package
    Add bcrypt npm package
    Create default user to seed database
    Create LoginForm.jsx
    Require authentication to use task form
    Add styles to login form
    Update server startup to include owners of tasks
    Add owners of tasks into the task form
    Add logout functionality

--- server/main.js
+++ server/main.js
@@ -1,18 +1,42 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { TasksCollection } from '/imports/api/TasksCollection';
 
-const insertTask = taskText => TasksCollection.insert({ text: taskText });
+// Insert new tasks into collection.
+const insertTask = (taskText, user) =>
+    TasksCollection.insert({
+        text: taskText,
+        userId: user._id,
+        createdAt: new Date()
+    });
 
+// Default user credentials.
+const SEED_USERNAME = 'gandalf';
+const SEED_PASSWORD = 'password';
+
+// Runs when server is started.
 Meteor.startup(() => {
-  if (TasksCollection.find().count() === 0) {
-    [
-      'First Task',
-      'Second Task',
-      'Third Task',
-      'Fourth Task',
-      'Fifth Task',
-      'Sixth Task',
-      'Seventh Task'
-    ].forEach(insertTask)
-  }
+    // Create the default user.
+    if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+        Accounts.createUser({
+            username: SEED_USERNAME,
+            password: SEED_PASSWORD,
+        });
+    }
+
+    // Create variable for the default user.
+    const user = Accounts.findUserByUsername(SEED_USERNAME);
+
+    // Insert default tasks.
+    if (TasksCollection.find().count() === 0) {
+        [
+            'First Task',
+            'Second Task',
+            'Third Task',
+            'Fourth Task',
+            'Fifth Task',
+            'Sixth Task',
+            'Seventh Task'
+        ].forEach(taskText => insertTask(taskText, user));
+    }
 });
